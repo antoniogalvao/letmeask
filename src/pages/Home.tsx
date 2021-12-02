d1ba8eb0de99d3cb1ade6ccb5ab5236db82f6762
Antonio Galvão <galvontony@gmail.com>
2021-12-02 20:04:38 -0300

    Add authentication context

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,22 @@
 import { useNavigate } from "react-router-dom";
-import { auth, firebase } from "../services/firebase";
+
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
 
-import "../styles/auth.scss";
+import { useAuth } from "../hooks/useAuth";
 import { Button } from "../components/Button";
+
+import "../styles/auth.scss";
 
 export function Home() {
   const navigate = useNavigate();
+  const { user, signInWithGoogle } = useAuth();
 
-  function handleCreateRoom() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    auth.signInWithPopup(provider).then((result) => {
-      console.log(result);
-    });
-
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
     navigate("/rooms/new");
   }
 
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
+
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 
-import "../styles/auth.scss";
 import { Button } from "../components/Button";
 
+import "../styles/auth.scss";
+import { useAuth } from "../hooks/useAuth";
+
 export function NewRoom() {
+  const { user } = useAuth();
+
   return (
     <div id="page-auth">
       <aside>
@@ -16,6 +21,7 @@ export function NewRoom() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="letmeask" />
+          <h1>{user?.name}</h1>
           <h2>Crie uma nova sala</h2>
           <form>
             <input type="text" placeholder="Nome da sala" />
